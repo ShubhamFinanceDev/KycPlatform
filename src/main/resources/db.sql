0ba4e3e6fb7a906329c182e1d99fb5a88e54c381
kajal@88 <kajal.01@dbalounge.com>
2024-01-19 12:16:00 +0530

    send otp api

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -32,44 +32,61 @@ CREATE TABLE IF NOT EXISTS `customer_details` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table rekyc.customer_details: ~2 rows (approximately)
-INSERT INTO `customer_details` (`user_id`, `application_number`, `loan_number`, `customer_name`, `address_details_residential`, `mobile_number`, `PAN`, `Aadhar`) VALUES
-	(1, '98765', 'LN003', 'nainish', '34 Main St, City', '8218372914', 'ABCDE1234F', 889831516204),
-	(2, '76756', 'LN005', 'kajal', '90 Main St, City', '8840961736', 'ABCuE1234F', 390920211147);
+-- Data exporting was unselected.
 
 -- Dumping structure for table rekyc.customer_details_seq
 CREATE TABLE IF NOT EXISTS `customer_details_seq` (
   `next_val` bigint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table rekyc.customer_details_seq: ~1 rows (approximately)
-INSERT INTO `customer_details_seq` (`next_val`) VALUES
-	(1);
+-- Data exporting was unselected.
 
 -- Dumping structure for table rekyc.otp_detail
 CREATE TABLE IF NOT EXISTS `otp_detail` (
+  `otp_id` bigint NOT NULL AUTO_INCREMENT,
   `expr_time` datetime(6) DEFAULT NULL,
   `mobile_number` varchar(255) DEFAULT NULL,
   `otp_code` bigint DEFAULT NULL,
-  `otp_id` bigint NOT NULL,
-  `otp_password` varchar(200) DEFAULT NULL,
+  `otp_password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`otp_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=653 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table rekyc.otp_detail: ~3 rows (approximately)
-INSERT INTO `otp_detail` (`expr_time`, `mobile_number`, `otp_code`, `otp_id`, `otp_password`) VALUES
-	('2024-01-09 18:35:59.000000', '8755776778', 590374, 602, NULL),
-	('2024-01-13 22:58:12.000000', NULL, 366009, 702, NULL),
-	('2024-01-15 20:15:24.000000', '8840961736', 961616, 852, '$2a$10$7slGtkQ.LojUuTuHhwrNQOW1kArQviSF6L3pxlg1Ns2O83jpR5wvS');
+-- Data exporting was unselected.
 
 -- Dumping structure for table rekyc.otp_detail_seq
 CREATE TABLE IF NOT EXISTS `otp_detail_seq` (
   `next_val` bigint DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table rekyc.otp_detail_seq: ~1 rows (approximately)
-INSERT INTO `otp_detail_seq` (`next_val`) VALUES
-	(951);
+-- Data exporting was unselected.
+
+-- Dumping structure for table rekyc.updated_details
+CREATE TABLE IF NOT EXISTS `updated_details` (
+  `update_id` bigint NOT NULL AUTO_INCREMENT,
+  `loan_no` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `document_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `document_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `updated_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `mobile_no` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `updated_date` date DEFAULT NULL,
+  PRIMARY KEY (`update_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table rekyc.uploaded_document
+CREATE TABLE IF NOT EXISTS `uploaded_document` (
+  `upload_id` bigint NOT NULL AUTO_INCREMENT,
+  `loan_no` varchar(255) DEFAULT NULL,
+  `document_id` varchar(255) DEFAULT NULL,
+  `document_type` varchar(255) DEFAULT NULL,
+  `front` varchar(255) DEFAULT NULL,
+  `back` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
+  PRIMARY KEY (`upload_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
