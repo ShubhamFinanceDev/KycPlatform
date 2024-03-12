f650555b7aff46a254b18eecd7748246d8e0c0e4
unknown <pmo_dbalounge@shubham.co>
2024-03-12 13:49:22 +0530

    new changes

--- src/main/java/com/example/reKyc/Controller/Admin.java
+++ src/main/java/com/example/reKyc/Controller/Admin.java
@@ -5,10 +5,7 @@ import com.example.reKyc.Repository.CustomerRepository;
 import org.apache.poi.openxml4j.util.ZipSecureFile;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -19,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
+@CrossOrigin
 public class Admin {
 
     @Autowired
