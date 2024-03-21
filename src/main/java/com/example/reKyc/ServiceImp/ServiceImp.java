1d9c83d5bcdc814720d603998db27d0819fe4bcb
nainish <pmo_dbalounge@shubham.co>
2024-03-21 15:06:14 +0530

    query update

--- src/main/java/com/example/reKyc/ServiceImp/ServiceImp.java
+++ src/main/java/com/example/reKyc/ServiceImp/ServiceImp.java
@@ -70,17 +70,17 @@ public class ServiceImp implements com.example.reKyc.Service.Service {
                 String phoneNo = saveCustomerDetails(customer.getLoanNumber());
                 int otpCode = otpUtility.generateOtp(phoneNo);
                 if (otpCode > 0 && saveOtpDetail(otpCode, phoneNo)) {
-//                  if (otpUtility.sendOtp(phoneNo, otpCode,loanNo)) {  //stopped sms services
+                  if (otpUtility.sendOtp(phoneNo, otpCode,loanNo)) {  //stopped sms services
                     logger.info("otp sent on mobile");
                     otpResponse.put("otpCode", String.valueOf(otpCode));
                     otpResponse.put("mobile", phoneNo);
                     otpResponse.put("msg", "Otp send.");
                     otpResponse.put("code", "0000");
 
-//                } else {
-//                    otpResponse.put("msg", "Otp did not send, please try again");
-//                    otpResponse.put("code", "1111");
-//                }
+                } else {
+                    otpResponse.put("msg", "Please try again");
+                    otpResponse.put("code", "1111");
+                }
 
                 } else {
                     otpResponse.put("msg", "Please try again");
@@ -202,24 +202,22 @@ public class ServiceImp implements com.example.reKyc.Service.Service {
 
 
     /**
+     *
      */
     @Override
     public CommonResponse updateCustomerKycFlag(String loanNo) {
 
         CommonResponse commonResponse = new CommonResponse();
         try {
-            Optional<Customer> customer = customerRepository.findById(loanNo);
-            if (customer.isPresent()) {
-                try {
+            try {
 
-                    customerRepository.updateKycFlag(loanNo);
-                    commonResponse.setMsg("Successfully");
-                    commonResponse.setCode("0000");
+                customerRepository.updateKycFlag(loanNo);
+                commonResponse.setMsg("Successfully");
+                commonResponse.setCode("0000");
 
-                } catch (Exception e) {
-                    commonResponse.setMsg("Flag did not updated.");
-                    commonResponse.setCode("1111");
-                }
+            } catch (Exception e) {
+                commonResponse.setMsg("Flag did not updated.");
+                commonResponse.setCode("1111");
             }
 
         } catch (Exception e) {
