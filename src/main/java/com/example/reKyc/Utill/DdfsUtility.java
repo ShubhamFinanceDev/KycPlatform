ac55fadb95855c80aeea19333c69d0b9d04f5694
kajalthakur99 <kajal.01@dbalounge.com>
2024-04-24 15:11:49 +0530

    change env

--- src/main/java/com/example/reKyc/Utill/DdfsUtility.java
+++ src/main/java/com/example/reKyc/Utill/DdfsUtility.java
@@ -87,14 +87,14 @@ public class DdfsUtility {
             formData.add("token", generateDDFSKey());
             formData.add("clientId", "SHUBHAM/OP");
             formData.add("file", applicationNo);
-//            formData.add("subPath", "2024/Aadhar");
-            formData.add("subPath",subPath);
+            formData.add("subPath", "2024/Aadhar");
+          //  formData.add("subPath",subPath);
             formData.add("docCategory", "IdentityProofs");
             formData.add("clientUserId", "06799");
             formData.add("remarks", "aadhar");
             formData.add("maker", "06799");
-//            formData.add("path", "HOBR/APF under-Constructi");
-            formData.add("path", path);
+            formData.add("path", "HOBR/APF under-Constructi");
+           // formData.add("path", path);
             formData.add("document", base64String);
 
             HttpHeaders headers = new HttpHeaders();
