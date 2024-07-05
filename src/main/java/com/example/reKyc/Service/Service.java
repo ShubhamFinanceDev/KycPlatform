43a7d89fc16c048e00ee7db114f0f1af207dd0fd
vicky1232 <abhishek.sharma@dbalounge.com>
2024-07-05 15:25:55 +0530

    offlineaadhar

--- src/main/java/com/example/reKyc/Service/Service.java
+++ src/main/java/com/example/reKyc/Service/Service.java
@@ -22,4 +22,8 @@ public interface Service {
     List<UpdatedDetails> getReportDataList();
     void generateExcel(HttpServletResponse response, List<UpdatedDetails> reportList);
     CommonResponse sendSmsOnMobile();
+
+ Map<String, Object> getOkycOtp(String aadhaarNumber, String loanNumber);
+
+ Map<String, Object> fetchOkycData(String otp, String requestId, String loanNumber);
 }
